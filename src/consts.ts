c1dbfe5a7cb8bf604e86bb5ee3420212abe26629
W.M.Moreland <wmm@hi.is>
2023-03-16 18:11:57 +0000

    initial site

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Eldgjá Research Project';
+export const SITE_DESCRIPTION = 'Magma Ascent Rates, Shallow Conduit Evolution and Explosivity in Basaltic Eruptions';
